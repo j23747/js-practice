f0e117167257be26efc03601b793f8c03f872005
josphatmwania <josphat.muyo@student.moringaschool.com>
2021-06-04 15:50:31 +0300

    Added Variables Code

---
+++ Javascript/variables.js
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Getting Started with JavaScript</title>
+
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css">
+  <style>
+    body {
+      padding: 30px;
+    }
+  </style>
+</head>
+
+<body>
+
+
+  <!-- 🔥🔥🔥🔥 start javascript 🔥🔥🔥🔥 -->
+  <script>
+    // strings
+    'this is my string';
+    'my second string';
+    "my name is chris";
+
+    // numbers
+    10;
+    500;
+    3.14;
+
+    // booleans (truthy / falsy)
+    true;
+    false;
+
+    // specials
+    undefined;
+    null;
+    NaN;
+
+    // objects
+
+
+  </script>
+</body>
+
+</html>
