28d0c8029e290e222567f125d17e6529e0c58421
josphatmwania <josphat.muyo@student.moringaschool.com>
2021-06-11 13:30:11 +0300

    Data structures practicve

---
+++ data-structures/index.js
@@ -0,0 +1,13 @@
+
+    const a = 10,
+    b = 3,
+    c = 15;
+    // Const strings
+const d = '5',
+    e = 'My name',
+    f =  'is Josphat';
+
+console.log(a + b);    
+console.log (a + c);
+console.log(e +  f);
+console.log(e + ' is a magican!');
